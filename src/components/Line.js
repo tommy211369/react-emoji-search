672f830b346eda070c3d3850d393296c80563c13
tommy211369 <tommy.thongrasamy@gmail.com>
2021-07-22 16:50:19 +0200

    hide message

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Line = (props) => {
   const { emoji, title } = props;
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="line">
-      {emoji} {title}
+    <div
+      className="line"
+      onMouseEnter={() => {
+        setShow(true);
+      }}
+      onMouseLeave={() => {
+        setShow(false);
+      }}
+    >
+      <div>
+        {emoji} {title}
+      </div>
+      <p className={show ? "visible" : "hidden"}>Click to copy !</p>
     </div>
   );
 };
